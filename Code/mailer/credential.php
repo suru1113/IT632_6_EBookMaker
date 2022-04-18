ec30a7dcb166bd9ed989e9b879bd473fb6e52c8c
Hardik Kaneriya <90904452+hardikdaiict@users.noreply.github.com>
2022-04-19 01:01:35 +0530

    Add Mailer Directory

---
+++ Code/mailer/credential.php
@@ -0,0 +1,4 @@
+<?php
+define('FROMEMAIL', 'onlinejobportal2021@gmail.com');
+define('PASS', 'OJP@2021');
+?>
---
+++ Code/mailer/index.php
@@ -0,0 +1,64 @@
+<?php
+//require 'PHPMailerAutoload.php';
+require 'credential.php';
+
+define('TOEMAIL', 'mrhardikdeveloper@gmail.com');
+
+function PHPMailerAutoload($classname)
+{
+    //Can't use __DIR__ as it's only in PHP 5.3+
+    $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
+    if (is_readable($filename)) {
+        require $filename;
+    }
+}
+
+if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
+    //SPL autoloading was introduced in PHP 5.1.2
+    if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
+        spl_autoload_register('PHPMailerAutoload', true, true);
+    } else {
+        spl_autoload_register('PHPMailerAutoload');
+    }
+} else {
+    function spl_autoload_register($classname)
+    {
+        PHPMailerAutoload($classname);
+    }
+}	
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 0;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = FROMEMAIL;                 // SMTP username
+$mail->Password = PASS;                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom(FROMEMAIL, 'E-Book Macker');
+$mail->addAddress(TOEMAIL, 'Joe User');     // Add a recipient
+// $mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo(FROMEMAIL, 'Replay To This Email');
+// $mail->addCC('cc@example.com');
+// $mail->addBCC('bcc@example.com');
+
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
